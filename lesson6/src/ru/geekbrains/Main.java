d318f80435a138bbf8d8b30fe149e51a1d057211
LuninDima <moon-di-2009@yandex.ru>
2021-02-11 14:49:42 +0300

    lesson 7

---
+++ lesson6/src/ru/geekbrains/Main.java
@@ -0,0 +1,14 @@
+package ru.geekbrains;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cat catBarsik = new Cat("Барсик", 100);
+	Dog dogSharik = new Dog("Шарик", 600, 11);
+
+
+ catBarsik.run();
+ dogSharik.run();
+ dogSharik.swim();
+    }
+}
