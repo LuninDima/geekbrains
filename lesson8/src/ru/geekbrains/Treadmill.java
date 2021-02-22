5cdb79129bbb72272135e76afddbd00af91e91b8
LuninDima <moon-di-2009@yandex.ru>
2021-02-22 13:59:37 +0300

    lesson 8

--- lesson8/src/ru/geekbrains/Treadmill.java
+++ lesson8/src/ru/geekbrains/Treadmill.java
@@ -1,6 +1,7 @@
 package ru.geekbrains;
 
 public class Treadmill {
+    int treadMillDistance = 15;
     public static void Running(String name, int distanceRun) {
         Moving.Run(name, distanceRun);
     }
---
+++ lesson9.1/src/ru/geekbrains/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains;
+
+public class MyArraySizeException extends RuntimeException{
+    public MyArraySizeException() {
+        super("Размер массива не корректный!");
+    }
+}
