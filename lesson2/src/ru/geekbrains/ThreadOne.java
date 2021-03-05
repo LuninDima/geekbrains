d3493dd02c1181eb156b924b63d1dd17f7c4abe2
LuninDima <moon-di-2009@yandex.ru>
2021-03-06 00:23:23 +0300

    lesson12

--- lesson2/src/ru/geekbrains/ThreadOne.java
+++ lesson2/src/ru/geekbrains/ThreadOne.java
@@ -15,7 +15,7 @@ public class ThreadOne extends Thread{
 
     @Override
     public void run() {
-//        System.out.println(Thread.currentThread().getName() + " start");
+
         calculate();
     }
 
@@ -25,6 +25,6 @@ public class ThreadOne extends Thread{
         for (int i = 0; i < len; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-//            System.out.println("End: " + Thread.currentThread().getName());
+
     }
 }
