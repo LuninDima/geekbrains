f3a1af08a07d52fe1286ec8ebbf673eaed7e2737
LuninDima <moon-di-2009@yandex.ru>
2021-03-06 20:17:40 +0300

    lesson10

--- lesson10/src/ru/geekbrains/Main.java
+++ lesson10/src/ru/geekbrains/Main.java
@@ -22,5 +22,35 @@ public class Main {
         for (String key : uniqueWord) {
             System.out.println(key + ": " + Collections.frequency(arrays, key));
         }
+
+
+        System.out.println("справочник");
+        Phonebook phonebook = new Phonebook();
+
+        phonebook.add("Человек1", "2342342345");
+        phonebook.add("Человек2", "53452345");
+        phonebook.add("Человек3", "2345345");
+        phonebook.add("Человек2", "6346");
+        phonebook.add("Человек1", "456456");
+        System.out.println("-----------------");
+
+        System.out.println("Получаем номера");
+        System.out.println("Человек1");
+        System.out.println(phonebook.get("Человек1"));
+        System.out.println("Человек2");
+        System.out.println(phonebook.get("Человек2"));
+        System.out.println("Человек3");
+        System.out.println(phonebook.get("Человек3"));
+        System.out.println("-----------------");
+
+        System.out.println("Если отсутствует в записи");
+        System.out.println("Человек4");
+        System.out.println(phonebook.get("Человек4"));
+        System.out.println("-----------------");
+
+        System.out.println("Запись существующего номера");
+        phonebook.add("Человек1", "2342342345");
+        System.out.println("Человек1");
+        System.out.println(phonebook.get("Человек1"));
     }
 }
