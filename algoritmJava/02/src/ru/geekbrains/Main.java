9a0e740f9025d77646c8f80ac351e455f049c493
LuninDima <moon-di-2009@yandex.ru>
2021-03-15 15:54:09 +0300

    lesson 2

---
+++ algoritmJava/02/src/ru/geekbrains/Main.java
@@ -0,0 +1,73 @@
+package ru.geekbrains;
+
+import javax.sound.midi.Soundbank;
+import java.lang.reflect.Array;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Random;
+
+import static ru.geekbrains.ElementToSearch.binaryFind;
+import static ru.geekbrains.ElementToSearch.linerFind;
+import static ru.geekbrains.Timer.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+      long timerStart = System.nanoTime();
+        int[] arrays = {1,2,3,4,5,6,7,8,9,10};
+        int[] arraysCopy;
+
+        Random rand = new Random();
+        for (int i = 0; i < arrays.length; i++) {
+        // arrays[i] = rand.nextInt(20);
+        }
+        arraysCopy = Arrays.copyOf(arrays, arrays.length);
+
+        System.out.println("Вывод адреса массива " + arrays.toString());
+        System.out.println("Вывод содержимого оригинального массива: " + Arrays.toString(arrays));
+        System.out.println("Вывод содержимого скопированного массива: " + Arrays.toString(arraysCopy));
+        System.out.println("Сравнение массивов " + Arrays.equals(arrays, arraysCopy));
+         long timerEnd = System.nanoTime();
+         long timer = (timerEnd - timerStart) / 1000000;
+        System.out.println("Время выполнения кода в мс: " + timer);
+
+
+        long timerStart2 = System.nanoTime();
+        System.out.println(linerFind(arrays, 3));
+        System.out.println(binaryFind(arrays, 3));
+        long timerEnd2 = System.nanoTime();
+        long timer2 = (timerEnd2 - timerStart2) / 1000000;
+        System.out.println("Время выполнения кода в мс: " + timer2);
+
+        int[] arrays400Elements = new int[400];
+        int[] arrays400ElementsCopy;
+        for (int i = 0; i < arrays400Elements.length; i++) {
+            arrays400Elements[i] = rand.nextInt(1000);
+        }
+        arrays400ElementsCopy = Arrays.copyOf(arrays400Elements, arrays400Elements.length);
+        long timerStart3 = System.nanoTime();
+        Arrays.sort(arrays400Elements);
+        System.out.println(Arrays.toString(arrays400Elements));
+        long timerEnd3 = System.nanoTime();
+        long timer3 = (timerEnd3 - timerStart3);
+        System.out.println("Время выполнения кода в нс: " + timer3);
+
+        BubbleSortArray array = new BubbleSortArray(arrays400ElementsCopy.length);
+        for (int i = 0; i < arrays400Elements.length; i++) {
+            array.into(arrays400ElementsCopy[i]);
+        }
+        System.out.println("Вывод неотсортированного массива: ");
+        array.printer();
+        System.out.println("Вывод отсортированного массива методом пузырьковой сортировки: ");
+        long timerStart4 = System.nanoTime();
+        array.bubbleSorted();
+        array.printer();
+        long timerEnd4 = System.nanoTime();
+        long timer4 = (timerEnd4 - timerStart4);
+        System.out.println("Время выполнения кода в нс: " + timer4);
+
+    }
+
+
+
+}
---
+++ algoritmJava/02/src/ru/geekbrains/Timer.java
@@ -0,0 +1,18 @@
+package ru.geekbrains;
+
+public class Timer {
+   public static long timerStart(){
+       long timerStart = System.nanoTime();
+       return timerStart;
+    }
+    public static long timerEnd(){
+        long timerEnd = System.nanoTime();
+        return timerEnd;
+    }
+    public static void timer(){
+        System.out.println(timerEnd() );
+        System.out.println(timerStart());
+        long timeRun = timerStart() - timerEnd();
+        System.out.println("Время выполнения кода: " + timeRun);
+    }
+}
